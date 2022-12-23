d03fb93924d2e88434d8501a30c21d60aad74452
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-12-23 00:20:27 -0300

    try resolve bug with cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,15 +1,8 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['http://localhost:1337', '186.216.221.6', 'https://strapi.hamiltonjr.dev']
-    }
-  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
